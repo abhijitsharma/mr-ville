69c2107d033a0e7051394e3bb4736cb8c2f3a44f
Abhijit Sharma <abhijit.sharma@gmail.com>
2012-08-03 07:58:59 +0100

    Changes

--- mr/src/test/java/examples/WordCountIT.java
+++ mr/src/test/java/examples/WordCountIT.java
@@ -5,18 +5,11 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MiniMRCluster;
-import org.apache.hadoop.mapreduce.CounterGroup;
 import org.apache.hadoop.mapreduce.Counters;
-import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,17 +27,16 @@ import java.io.InputStreamReader;
  */
 public class WordCountIT {
 
-    private static Path TEST_ROOT_DIR =
-            new Path(System.getProperty("test.build.data", "testOut"));
-    
-    private static Configuration conf = new Configuration();
-    private static FileSystem localFs;
+    private static Path TEST_DIR = new Path("target" + File.separatorChar + "testOut");
+
+    private static Configuration config = new Configuration();
+    private static FileSystem lfs;
 
     static {
         try {
-            localFs = FileSystem.getLocal(conf);
+            lfs = FileSystem.getLocal(config);
         } catch (IOException io) {
-            throw new RuntimeException("problem getting local fs", io);
+            throw new RuntimeException("Error : Local fs", io);
         }
     }
 
@@ -52,13 +44,6 @@ public class WordCountIT {
     public void setUp() {
     }
 
-//    @Test
-    public void test() throws Exception{
-        File f = new File("E:\\code\\mr-ville\\mr\\.\\userlogs\\job_20120406191009873_0001\\attempt_20120406191009873_0001_m_000002_1");
-        if(!f.mkdir()) {
-            throw new IOException("Mkdirs failed to create "                     + f.toString());
-        }
-    }
     @Test
     public void testRun() throws Exception {
         MiniMRCluster mr = null;
@@ -74,17 +59,17 @@ public class WordCountIT {
 
     }
 
-    public static Path writeFile(String name, String data) throws IOException {
-        Path file = new Path(TEST_ROOT_DIR + "/" + name);
-        localFs.delete(file, false);
-        DataOutputStream f = localFs.create(file);
+    public static Path writeFile(FileSystem _lfs, String path, String data) throws IOException {
+        Path file = new Path(path);
+        _lfs.delete(file, false);
+        DataOutputStream f = _lfs.create(file);
         f.write(data.getBytes());
         f.close();
         return file;
     }
 
-    public static String readFile(String name) throws IOException {
-        DataInputStream f = localFs.open(new Path(TEST_ROOT_DIR + "/" + name));
+    public static String readFile(FileSystem _lfs, String path) throws IOException {
+        DataInputStream f = _lfs.open(new Path(path));
         BufferedReader b = new BufferedReader(new InputStreamReader(f));
         StringBuilder result = new StringBuilder();
         String line = b.readLine();
@@ -97,44 +82,11 @@ public class WordCountIT {
         return result.toString();
     }
 
-    public static class TrackingTextInputFormat extends TextInputFormat {
-
-        public static class MonoProgressRecordReader extends LineRecordReader {
-            private float last = 0.0f;
-            private boolean progressCalled = false;
-
-            @Override
-            public float getProgress() {
-                progressCalled = true;
-                final float ret = super.getProgress();
-                Assert.assertTrue("getProgress decreased", ret >= last);
-                last = ret;
-                return ret;
-            }
-
-            @Override
-            public synchronized void close() throws IOException {
-                Assert.assertTrue("getProgress never called", progressCalled);
-                super.close();
-            }
-        }
-
-        @Override
-        public RecordReader<LongWritable, Text> createRecordReader(
-                InputSplit split, TaskAttemptContext context) {
-            return new MonoProgressRecordReader();
-        }
-    }
-
-    private void runWordCount(Configuration conf
-    ) throws IOException,
-            InterruptedException,
-            ClassNotFoundException {
-        final String COUNTER_GROUP = "org.apache.hadoop.mapred.Task$Counter";
-        localFs.delete(new Path(TEST_ROOT_DIR + "/in"), true);
-        localFs.delete(new Path(TEST_ROOT_DIR + "/out"), true);
-        writeFile("in/part1", "this is a test\nof word count test\ntest\n");
-        writeFile("in/part2", "more test");
+    private void runWordCount(Configuration conf) throws Exception {
+        lfs.delete(new Path(TEST_DIR + File.separator + "in"), true);
+        lfs.delete(new Path(TEST_DIR + File.separator + "out"), true);
+        writeFile(lfs, TEST_DIR + File.separator + "in/part1", "this is a test\nof word count test\ntest\n");
+        writeFile(lfs, TEST_DIR + File.separator + "in/part2", "more test");
         Job job = new Job(conf, "word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(WordCount.Map.class);
@@ -142,37 +94,17 @@ public class WordCountIT {
         job.setReducerClass(WordCount.Reduce.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setInputFormatClass(TrackingTextInputFormat.class);
-        FileInputFormat.addInputPath(job, new Path(TEST_ROOT_DIR + "/in"));
-        FileOutputFormat.setOutputPath(job, new Path(TEST_ROOT_DIR + "/out"));
+        FileInputFormat.addInputPath(job, new Path(TEST_DIR + "/in"));
+        FileOutputFormat.setOutputPath(job, new Path(TEST_DIR + "/out"));
         Assert.assertNull(job.getJobID());
         Assert.assertTrue(job.waitForCompletion(false));
         Assert.assertNotNull(job.getJobID());
-        String out = readFile("out/part-r-00000");
+        String out = readFile(lfs, TEST_DIR + File.separator + "out/part-r-00000");
         System.out.println(out);
         Assert.assertEquals("a\t1\ncount\t1\nis\t1\nmore\t1\nof\t1\ntest\t4\nthis\t1\nword\t1\n",
                 out);
         Counters ctrs = job.getCounters();
         System.out.println("Counters: " + ctrs);
-        long combineIn = ctrs.findCounter(COUNTER_GROUP,
-                "COMBINE_INPUT_RECORDS").getValue();
-        long combineOut = ctrs.findCounter(COUNTER_GROUP,
-                "COMBINE_OUTPUT_RECORDS").getValue();
-        long reduceIn = ctrs.findCounter(COUNTER_GROUP,
-                "REDUCE_INPUT_RECORDS").getValue();
-        long mapOut = ctrs.findCounter(COUNTER_GROUP,
-                "MAP_OUTPUT_RECORDS").getValue();
-        long reduceOut = ctrs.findCounter(COUNTER_GROUP,
-                "REDUCE_OUTPUT_RECORDS").getValue();
-        long reduceGrps = ctrs.findCounter(COUNTER_GROUP,
-                "REDUCE_INPUT_GROUPS").getValue();
-        Assert.assertEquals("map out = combine in", mapOut, combineIn);
-        Assert.assertEquals("combine out = reduce in", combineOut, reduceIn);
-        Assert.assertTrue("combine in > combine out", combineIn > combineOut);
-        Assert.assertEquals("reduce groups = reduce out", reduceGrps, reduceOut);
-        String group = "Random Group";
-        CounterGroup ctrGrp = ctrs.getGroup(group);
-        Assert.assertEquals(0, ctrGrp.size());
     }
 
 }
